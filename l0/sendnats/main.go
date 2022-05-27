44cdcae1ab2eca1baca2255a0642d5dd082c73b4
Aleksandr <a.y.ivlev@gmail.com>
2022-05-27 08:42:26 +0300

    fix in send nats.

--- l0/sendnats/main.go
+++ l0/sendnats/main.go
@@ -18,32 +18,12 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-		time.Sleep(10*time.Second)
-		err = sc.Publish(channelName, message[0])
+
+	for _, msg := range message {
+		time.Sleep(10 * time.Second)
+		err = sc.Publish(channelName, msg)
 		if err != nil {
 			log.Println(err)
 		}
-		time.Sleep(10*time.Second)
-
-		err = sc.Publish(channelName, message[1])
-		if err != nil {
-			log.Println(err)
-		}
-		time.Sleep(15*time.Second)
-
-		err = sc.Publish(channelName, message[2])
-		if err != nil {
-			log.Println(err)
-		}
-		time.Sleep(20*time.Second)
-
-		err = sc.Publish(channelName, message[3])
-		if err != nil {
-			log.Println(err)
-	 	}
-		time.Sleep(25*time.Second)
-		err = sc.Publish(channelName, message[4])
-		if err != nil {
-			log.Println(err)
-	 	} 	
+	}
 }
